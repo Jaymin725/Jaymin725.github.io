49b2f958b7c860088197be44c74a6c2f36e14f0b
Jaymin Bhavsar <jayminbhavsar07@gmail.com>
2023-12-03 00:11:18 +0530

    added typed.js

---
+++ scripts/typed.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#msg', {
+  strings: ['I\'m a <i class="gradient-text">Front-End</i> Developer', 'I\'m a <span class="gradient-text" style="font-family: MajorMonoDisplay;">Web Designer</span>', 'I\'m a <strong class="gradient-text">Web Developer</strong>'],
+  typeSpeed: 50,
+  backSpeed: 35,
+  cursorChar: '_',
+  loop: true
+});
