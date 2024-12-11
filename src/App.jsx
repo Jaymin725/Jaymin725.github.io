54c212c224fcc75a899c72f744021b4b20ef7f79
Jaymin Bhavsar <jayminbhavsar07@gmail.com>
2024-12-11 06:36:14 +0530

    Setup React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+// Pages
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
